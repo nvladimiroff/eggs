6db808f49f6f7d64501ba2efdfc6f4a05ca0b31b
Nick Vladimiroff <nickvladimiroff@gmail.com>
2021-12-27 23:04:09 -0500

    Initial gem!

---
+++ lib/eggs/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Eggs
+  VERSION = '0.0.1'
+end
---
+++ test/eggs_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class EggsTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Eggs::VERSION
+  end
+
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
